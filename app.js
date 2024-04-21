b9b41a26c04d94dce420a89cbdd166cfea94bc91
Fahad Ghaffar <gayesha759@gmail.com>
2024-04-21 23:53:05 +0500

    update

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 alert("Welcome to our website! We're glad you're here.");
+alert("Error! Please Enter a Valid password");
+console.log("hello...i can run js through browser's developer console");
+alert("Welcome to Js land \n happy coding");
